3dfde2b5212b947241151ed519893207e020aae1
Dominik Juriga <145474492+dominik-juriga@users.noreply.github.com>
2026-06-17 14:17:09 +0200

    Chore/optimize package json (#2)
    * chore: update projectId references
    
    * chore: pin dependency versions
    
    * chore: add repo info to package json
    
    * fix: smart populate text in banner
    
    * fix: banner images with shorter name
    
    * feat: update readme with know issue
    
    ---------
    
    Co-authored-by: Libor Riha <libor.riha@notum.cz>

--- admin/src/extensions/ContentTypeBuilderBanner.tsx
+++ admin/src/extensions/ContentTypeBuilderBanner.tsx
@@ -38,7 +38,7 @@ const ContentTypeBuilderBanner = () => {
   const [bannerRoot, setBannerRoot] = useState<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    get<OverridesResponse>('/strapi-plugin-smart-populate/overrides')
+    get<OverridesResponse>('/smart-populate/overrides')
       .then(({ data }) => setOverrides(data.data))
       .catch(() => setOverrides([]));
   }, [get]);
@@ -132,7 +132,7 @@ const ContentTypeBuilderBanner = () => {
                 </Typography>{' '}
                 and adjust{' '}
                 <Typography variant="epsilon" fontWeight="bold">
-                  <code>strapi-plugin-smart-populate.config.populateOverrides</code>
+                  <code>smart-populate.config.populateOverrides</code>
                 </Typography>
                 .
               </Typography>
